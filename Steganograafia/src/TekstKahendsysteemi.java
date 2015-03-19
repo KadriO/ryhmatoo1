5cc14735cc42738f8caae82b121e76da1419e98c
liisi <liisi@arvuti>
2015-03-19 16:15:30 +0200

    parandused

--- Steganograafia/src/TekstKahendsysteemi.java
+++ Steganograafia/src/TekstKahendsysteemi.java
@@ -6,23 +6,23 @@ public class TekstKahendsysteemi {
 		s = "¤" + s + "~";
 		
 		//Teeme baidimassiivi
-		byte[] bytes = s.getBytes();
-		StringBuilder binary = new StringBuilder();
+		byte[] baidid = s.getBytes();
+		StringBuilder kahendkood = new StringBuilder();
 		// Käime for-tsükliga üle iga baidi (tähe); iga baidi biti puhul kontrollime, kas see on seatud(1) või mitte(0) 
 		// selleks kasutame AND-tehet arvuga 128 (kahendsüsteemis 10000000)
 		// kui tehte tulemus on 1, siis on vasakpoolseim bitt seatud, vastasel juhul mitte.
 		// edasi nihutame baidi bitte vasakule, saadud baidis on nüüd järgmine bitt kõige olulisem
 		// nõnda saame teada, kas järgmine bitt on seatud ja nii edasi, kuni saamegi kokku panna
 		// baidile vastava kahendkoodi
-		for (byte b : bytes) {
+		for (byte b : baidid) {
 			int v22rtus = b;
 			
 			for (int i = 0; i < 8; i++) {
-				binary.append((v22rtus & 128) == 0 ? 0 : 1);
+				kahendkood.append((v22rtus & 128) == 0 ? 0 : 1);
 				v22rtus <<= 1;
 		     }
-		     binary.append(' ');
+		     kahendkood.append(' ');
 		  }
-		  return binary.toString();
+		  return kahendkood.toString();
 	}
 }
